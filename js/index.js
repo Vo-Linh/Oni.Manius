38b1e83fbb98af953cb14bb394cebae912021427
Vo-Linh <volinh0607@gmail.com>
2021-04-30 22:21:13 +0700

    done work

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 // ===================Fullpage============
-new fullpage("#fullpage",{
+new fullpage("#fullpage", {
     //options here
     licenseKey: null,
-	autoScrolling:true,
-	scrollHorizontally: true
+    autoScrolling: true,
+    scrollHorizontally: true
 });
 
 // ===============Owl=======================
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:40,
-    nav:true,
-	items:3,
+    loop: true,
+    margin: 40,
+    nav: true,
+    items: 3,
     // responsive:{
     //     0:{
     //         items:1
@@ -25,3 +25,28 @@ $('.owl-carousel').owlCarousel({
     // }
 });
 
+// =================slider========================
+
+$(document).ready(function () {
+    for (var i = 1; i <= $('.slider__slide').length; i++) {
+        $('.slider__indicators').append('<div class="slider__indicator" data-slide="' + i + '"></div>')
+    }
+    setTimeout(function () {
+        $('.slider__wrap').addClass('slider__wrap--hacked');
+    }, 1000);
+})
+
+function goToSlide(number) {
+    $('.slider__slide').removeClass('slider__slide--active');
+    $('.slider__slide[data-slide=' + number + ']').addClass('slider__slide--active');
+}
+
+$('.slider__next, .go-to-next').on('click', function () {
+    var currentSlide = Number($('.slider__slide--active').data('slide'));
+    var totalSlides = $('.slider__slide').length;
+    currentSlide++
+    if (currentSlide > totalSlides) {
+        currentSlide = 1;
+    }
+    goToSlide(currentSlide);
+})
