8011161a1d88b52e84d67d8267dcf3c8b2584852
Vo-Linh <volinh0607@gmail.com>
2021-05-25 16:04:54 +0700

    work

--- js/main.js
+++ js/main.js
@@ -45,100 +45,20 @@ $(window).on('load', function () {
   });
 })
 $(document).ready(function () {
-  $('.anh a').fancybox();
+  $('.anh a').fancybox({
+    // toolbar: false,
+  });
+
 });
 
 
-//  ===============silder=====================
+//  ===============back to top=====================
+backtop = document.getElementById("backtop");
 
-(function () {
-  var $slides = document.querySelectorAll(".slide");
-  var $controls = document.querySelectorAll(".slider__control");
-  var numOfSlides = $slides.length;
-  var slidingAT = 1300; // sync this with scss variable
-  var slidingBlocked = false;
-
-  [].slice.call($slides).forEach(function ($el, index) {
-    var i = index + 1;
-    $el.classList.add("slide-" + i);
-    $el.dataset.slide = i;
-  });
-
-  [].slice.call($controls).forEach(function ($el) {
-    $el.addEventListener("click", controlClickHandler);
-  });
-
-  function controlClickHandler() {
-    if (slidingBlocked) return;
-    slidingBlocked = true;
-
-    var $control = this;
-    var isRight = $control.classList.contains("m--right");
-    var $curActive = document.querySelector(".slide.s--active");
-    var index = +$curActive.dataset.slide;
-    isRight ? index++ : index--;
-    if (index < 1) index = numOfSlides;
-    if (index > numOfSlides) index = 1;
-    var $newActive = document.querySelector(".slide-" + index);
-
-    $control.classList.add("a--rotation");
-    $curActive.classList.remove("s--active", "s--active-prev");
-    document.querySelector(".slide.s--prev").classList.remove("s--prev");
-
-    $newActive.classList.add("s--active");
-    if (!isRight) $newActive.classList.add("s--active-prev");
-
-    var prevIndex = index - 1;
-    if (prevIndex < 1) prevIndex = numOfSlides;
-
-    document.querySelector(".slide-" + prevIndex).classList.add("s--prev");
-
-    setTimeout(function () {
-      $control.classList.remove("a--rotation");
-      slidingBlocked = false;
-    }, slidingAT * 0.75);
-  }
-})();
-
-document.addEventListener("DOMContentLoaded", function () {
-  var controls = document.querySelectorAll(".slider__control");
-  // console.log(controls)
-  var slides = document.querySelectorAll(".slide");
-  // console.log(slides);
-  auto();
-
-  function auto() {
-    var thoigian = setInterval(function () {
-      var slide = document.querySelector(".slide.s--active");
-      // console.log(slide);  
-      var vitrislide = 0;
-      for (var i = 0; slide = slide.previousElementSibling; vitrislide++) {}
-      for (var i = 0; i < slides.length; i++) {
-        slides[i].classList.remove('s--active');
-      }
-      if (vitrislide == slides.length - 1) {
-        slides[0].classList.add('s--active');
-      } else {
-        slides[vitrislide].nextElementSibling.classList.add('s--active');
-        // console.log(slide);
-      }
-    }, 3000)
-    // Tu dong chuyen slide
-    for (var i = 0; i < console.length; i++) {
-      // console.log(controls[i])
-      // controls[i].addEventListener('click', function () {
-      //   clearInterval(thoigian);
-      // })
-    }
-    [].slice.call(controls).forEach(function ($el) {
-      $el.addEventListener("click", () => {
-        clearInterval(thoigian);
-      });
-    });
-    // Dung tu dong chuyen slide
-  }
-
-}, false)
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 // ===================================
 // *
@@ -162,7 +82,7 @@ $('.owl-carousel').owlCarousel({
   margin: 40,
   // nav: true,
   items: 3,
-  autoplay:true,
+  autoplay: true,
   autoplayTimeout: 1000,
   // responsive:{
   //     0:{
@@ -183,11 +103,11 @@ const items = document.querySelectorAll(".accordion button");
 
 function toggleAccordion() {
   const itemToggle = this.getAttribute('aria-expanded');
-  
+
   for (i = 0; i < items.length; i++) {
     items[i].setAttribute('aria-expanded', 'false');
   }
-  
+
   if (itemToggle == 'false') {
     this.setAttribute('aria-expanded', 'true');
   }
